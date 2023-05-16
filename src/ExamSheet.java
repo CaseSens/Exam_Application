f08227bac1fb3057a1fbc2e856794a0a3d5c0ed1
Casey <caseyggareau@gmail.com>
2023-05-16 16:16:57 -0400

    Version 6, ExamSheet now has the capabilities of adding: Multiple Choice questions Short Answer Questions Long Answer Questions
    it also automatically refreshes Q#, so that it always remains 123456...

--- src/ExamSheet.java
+++ src/ExamSheet.java
@@ -3,8 +3,16 @@ import java.awt.*;
 
 public class ExamSheet extends AdminPage{
     JFrame examSheet;
-    JPanel centerPanelForExamQuestions;
     int questionIncrement=1;
+    private static final GridBagConstraints gbc;
+    static {
+        gbc = new GridBagConstraints();
+        gbc.gridx = 0;
+        gbc.gridy = GridBagConstraints.RELATIVE;
+        gbc.fill = GridBagConstraints.HORIZONTAL;
+        gbc.insets = new Insets(5, 5, 80, 5);
+    }
+    private static final Dimension questionTextDimensions = new Dimension(300, 65);
 
     ExamSheet () {
         createExamSheet();
@@ -48,41 +56,26 @@ public class ExamSheet extends AdminPage{
         centerLeftPanel.add(addMenu, BorderLayout.WEST);
         centerLeftPanel.setBackground(Color.cyan);
 
-        centerPanelForExamQuestions = new JPanel();
-        centerPanelForExamQuestions.setLayout(new BoxLayout(centerPanelForExamQuestions, BoxLayout.Y_AXIS));
-        centerPanelForExamQuestions.add(Box.createVerticalGlue());
+        //----------------------------------------------------------------------
 
-
+        JScrollPane examScrollPane = new JScrollPane();
+        examScrollPane.setPreferredSize(new Dimension(1600, 900));
+        JPanel allQuestionsContainer = new JPanel();
+        allQuestionsContainer.setLayout(new GridBagLayout());
 
         addMultipleChoice.addActionListener(e -> {
-            JPanel exam = new JPanel();
-            exam.setMaximumSize(new Dimension(200, 300)); //sets width to 200, but for some reason doesnt work on height
-            exam.setPreferredSize(new Dimension(200, 250)); //sets height to 300, but for some reason doesnt work on width
-            exam.setLayout(new BoxLayout(exam, BoxLayout.Y_AXIS));
-            JPanel questionAnswer = new JPanel();
-            JLabel question = new JLabel(questionIncrement + ". Enter question ");
-            JTextField questionInput = new JTextField();
-            JButton removeQuestion = new JButton("Remove Question");
-
-//            exam.add(questionAnswer);
-            questionAnswer.add(question);
-            questionAnswer.add(questionInput);
-            exam.add(questionAnswer);
-
-            for (int i = 0; i < 4; i++) {
-                JTextField answers = new JTextField();
-                exam.add(answers);
-            }
-            centerPanelForExamQuestions.setLayout(new BoxLayout(centerPanelForExamQuestions, BoxLayout.Y_AXIS));
-            centerPanelForExamQuestions.setAlignmentY(Component.TOP_ALIGNMENT);
-            centerPanelForExamQuestions.add(Box.createVerticalGlue());
-            centerPanelForExamQuestions.add(exam);
-            centerPanelForExamQuestions.revalidate();
-            questionIncrement++;
+            createMultipleChoice(allQuestionsContainer);
         });
 
-        JScrollPane examScrollPane = new JScrollPane(centerPanelForExamQuestions);
-        examScrollPane.setPreferredSize(new Dimension(1920, 1080));
+        addShortAnswer.addActionListener(e -> {
+            createShortAnswer(allQuestionsContainer);
+        });
+
+        addLongAnswer.addActionListener(e -> {
+            createLongAnswer(allQuestionsContainer);
+        });
+
+        examScrollPane.setViewportView(allQuestionsContainer);
         examScrollPane.getVerticalScrollBar().setUnitIncrement(16);
 
         examSheet = new JFrame("Create your exam");
@@ -91,5 +84,170 @@ public class ExamSheet extends AdminPage{
         examSheet.setSize(1600, 900);
         examSheet.setLocationRelativeTo(null);
         examSheet.setVisible(true);
+    }
+
+    private void createLongAnswer(JPanel allQuestionsContainer) {
+        JPanel questionContainer = new JPanel(new GridBagLayout());
+        JPanel questionPanel = new JPanel();
+        JLabel question = new JLabel("Q" + questionIncrement + ": ");
+        JTextArea questionInput = new JTextArea();
+        questionInput.setLineWrap(true);
+        questionInput.setWrapStyleWord(true);
+        JScrollPane questionTextPane = new JScrollPane(questionInput);
+        questionTextPane.setVerticalScrollBarPolicy(JScrollPane.VERTICAL_SCROLLBAR_AS_NEEDED);
+        questionTextPane.setHorizontalScrollBarPolicy(JScrollPane.HORIZONTAL_SCROLLBAR_NEVER);
+        questionTextPane.setPreferredSize(new Dimension(500, 250));
+        JButton removeQuestion = new JButton("Remove question");
+        GridBagConstraints c = new GridBagConstraints();
+        c.fill = GridBagConstraints.HORIZONTAL;
+        c.gridx = 0; //set 0 columns
+        c.gridy = GridBagConstraints.RELATIVE; //set row to 0, increment as components are added
+        c.insets = new Insets(5, 5, 5, 5); //padding between components
+
+        questionPanel.setLayout(new FlowLayout());
+        questionPanel.add(question);
+        questionPanel.add(questionTextPane);
+        questionPanel.add(removeQuestion);
+
+        questionContainer.add(questionPanel, c);
+
+        allQuestionsContainer.add(questionContainer, gbc);
+        questionIncrement++;
+        System.out.println(questionIncrement);
+        examSheet.revalidate();
+
+        removeQuestion.addActionListener(e1 -> {
+            allQuestionsContainer.remove(questionContainer);
+            questionIncrement--;
+            System.out.println(questionIncrement);
+            refreshQuestionNumbers(allQuestionsContainer);
+            allQuestionsContainer.revalidate();
+            allQuestionsContainer.repaint();
+            examSheet.revalidate();
+        });
+    }
+
+    private void createShortAnswer(JPanel allQuestionsContainer) {
+        JPanel questionContainer = new JPanel(new GridBagLayout());
+        JPanel questionPanel = new JPanel();
+        JLabel question = new JLabel("Q" + questionIncrement + ": ");
+        JTextArea questionInput = new JTextArea();
+        questionInput.setLineWrap(true);
+        questionInput.setWrapStyleWord(true);
+        JScrollPane questionTextPane = new JScrollPane(questionInput);
+        questionTextPane.setVerticalScrollBarPolicy(JScrollPane.VERTICAL_SCROLLBAR_AS_NEEDED);
+        questionTextPane.setHorizontalScrollBarPolicy(JScrollPane.HORIZONTAL_SCROLLBAR_NEVER);
+        questionTextPane.setPreferredSize(questionTextDimensions);
+        JButton removeQuestion = new JButton("Remove question");
+        GridBagConstraints c = new GridBagConstraints();
+        c.fill = GridBagConstraints.HORIZONTAL;
+        c.gridx = 0; //set 0 columns
+        c.gridy = GridBagConstraints.RELATIVE; //set row to 0, increment as components are added
+        c.insets = new Insets(5, 5, 5, 5); //padding between components
+
+        questionPanel.setLayout(new FlowLayout());
+        questionPanel.add(question);
+        questionPanel.add(questionTextPane);
+        questionPanel.add(removeQuestion);
+
+        questionContainer.add(questionPanel, c);
+
+        allQuestionsContainer.add(questionContainer, gbc);
+        questionIncrement++;
+        System.out.println(questionIncrement);
+        examSheet.revalidate();
+
+        removeQuestion.addActionListener(e1 -> {
+            allQuestionsContainer.remove(questionContainer);
+            questionIncrement--;
+            System.out.println(questionIncrement);
+            refreshQuestionNumbers(allQuestionsContainer);
+            allQuestionsContainer.revalidate();
+            allQuestionsContainer.repaint();
+            examSheet.revalidate();
+        });
+    }
+
+    private void createMultipleChoice(JPanel allQuestionsContainer) {
+        JPanel questionContainer = new JPanel(new GridBagLayout());
+        JPanel questionPanel = new JPanel();
+        JPanel answerPanel = new JPanel();
+        JLabel question = new JLabel("Q" + questionIncrement + ": ");
+        JTextArea questionInput = new JTextArea();
+        questionInput.setLineWrap(true);
+        questionInput.setWrapStyleWord(true);
+        JScrollPane questionTextPane = new JScrollPane(questionInput);
+        questionTextPane.setVerticalScrollBarPolicy(JScrollPane.VERTICAL_SCROLLBAR_AS_NEEDED);
+        questionTextPane.setHorizontalScrollBarPolicy(JScrollPane.HORIZONTAL_SCROLLBAR_NEVER);
+        Dimension multipleAnswerDimensions = new Dimension(300, 40);
+        questionTextPane.setPreferredSize(new Dimension(questionTextDimensions));
+        JButton removeQuestion = new JButton("Remove question");
+        GridBagConstraints c = new GridBagConstraints();
+        c.fill = GridBagConstraints.HORIZONTAL;
+        c.gridx = 0; //set 0 columns
+        c.gridy = GridBagConstraints.RELATIVE; //set row to 0, increment as components are added
+        c.insets = new Insets(5, 5, 5, 5); //padding between components
+
+        questionPanel.setLayout(new FlowLayout());
+        questionPanel.add(question);
+        questionPanel.add(questionTextPane);
+        questionPanel.add(removeQuestion);
+
+        questionContainer.add(questionPanel, c);
+
+        char currentCharacter = 'A';
+        for (int i=0; i<4; i++) {
+
+            JLabel abcd = new JLabel(String.valueOf(currentCharacter) + ": ");
+            JTextField answerInput = new JTextField();
+            answerInput.setPreferredSize(new Dimension(multipleAnswerDimensions));
+            answerPanel.add(abcd);
+            answerPanel.add(answerInput);
+            questionContainer.add(answerPanel, c);
+            currentCharacter++;
+        }
+
+        allQuestionsContainer.add(questionContainer, gbc);
+        questionIncrement++;
+        System.out.println(questionIncrement);
+        examSheet.revalidate();
+
+        removeQuestion.addActionListener(e1 -> {
+            allQuestionsContainer.remove(questionContainer);
+            questionIncrement--;
+            System.out.println(questionIncrement);
+            refreshQuestionNumbers(allQuestionsContainer);
+            allQuestionsContainer.revalidate();
+            allQuestionsContainer.repaint();
+            examSheet.revalidate();
+        });
+    }
+
+    private void refreshQuestionNumbers(JPanel allQuestionsContainer) {
+        Component[] components = allQuestionsContainer.getComponents(); //each questionContainer
+        for (int i = 0; i < components.length; i++) { //loop through them
+            if (components[i] instanceof JPanel) { //if component is JPanel
+                JPanel panel = (JPanel) components[i]; //create a new JPanel from that component
+                Component[] innerComponents = panel.getComponents(); //each components of questionContainer
+                for (Component component : innerComponents) { //iterate through the inner components
+                    if (component instanceof JPanel) {
+                        JPanel newQuestionPanel = (JPanel) component;
+                        Component[] questionComponents = newQuestionPanel.getComponents();
+                        for (Component questionComponent : questionComponents) {
+                            if (questionComponent instanceof JLabel) { //if its a JLabel
+                                JLabel label = (JLabel) questionComponent; //create new Label from the component
+                                String labelText = label.getText(); //get text from the Label
+                                if (labelText.startsWith("Q")) { //only get Labels that start with Q
+                                    int questionNumber = Integer.parseInt(labelText.substring(1, labelText.indexOf(":"))); //grab only the number
+                                    if (questionNumber > questionIncrement - 1) {
+                                        label.setText("Q" + (questionNumber - 1) + ": "); //set question - 1
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
     }
 }
